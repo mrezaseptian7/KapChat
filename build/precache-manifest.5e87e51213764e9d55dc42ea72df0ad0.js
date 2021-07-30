5fcb74686bab36174f47407e293f8a16b9f2a4bf
mrezaseptian7 <87309851+mrezaseptian7@users.noreply.github.com>
2021-07-30 21:42:33 +0700

    Add files via upload

---
+++ build/precache-manifest.5e87e51213764e9d55dc42ea72df0ad0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a28608abf79e7effc3e547ef1dd8d7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3e3d114c266c4506f8f",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "395752295ebecc0d8372",
+    "url": "/static/css/main.8c91fcea.chunk.css"
+  },
+  {
+    "revision": "b3e3d114c266c4506f8f",
+    "url": "/static/js/2.93ae1817.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.93ae1817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "395752295ebecc0d8372",
+    "url": "/static/js/main.573e4d42.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
